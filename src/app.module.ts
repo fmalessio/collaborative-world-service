9b0bf46072bff05ddf86012796b096b4cb03cded
fmalessio <fmalessio@gmail.com>
2020-10-30 19:34:17 -0300

    DB: FK custom naming

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,21 @@ import { AppController } from './app.controller';
 import { CategoryModule } from './category/category.module';
 import { DonationModule } from './donation/donation.module';
 import { GeolocationModule } from './geolocation/geolocation.module';
+import { CustomNamingStrategy } from './shared/db/CustomNamingStrategy';
 require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
+      synchronize: true,
+      namingStrategy: new CustomNamingStrategy(),
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: ['dist/**/*.entity.js'],
-      synchronize: true,
       logging: Boolean(process.env.DB_LOGGING)
     }),
     ConfigModule.forRoot(),
